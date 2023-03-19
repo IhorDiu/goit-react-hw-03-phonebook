68f80e03281b52ebb0243068e38c054cd355996c
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-03-19 22:38:53 +0200

    LocalStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,9 +8,26 @@ import { Title, Subtitle } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: initialContacts,
+    contacts: [],
     filter: '',
   };
+
+componentDidMount() {
+  const savedContacts = localStorage.getItem('contacts');
+  if (savedContacts !== null) {
+    const parsedContacts = JSON.parse(savedContacts);
+    this.setState({ contacts: parsedContacts});
+    return;
+    }
+    this.setState({contacts: initialContacts});
+  }
+
+
+componentDidUpdate(prevProps, prevState) {
+  if (prevState.contacts !== this.state.contacts) {
+  localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+}
 
   addContact = newContact => {
     if (
